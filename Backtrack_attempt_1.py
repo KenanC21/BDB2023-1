a0fd3a1af519dabb0d7d1db10a7cb672e008e965
KenanC21 <66731065+KenanC21@users.noreply.github.com>
2022-12-19 15:43:56 -0500

    Update Backtrack_attempt_1.py

--- Backtrack_attempt_1.py
+++ Backtrack_attempt_1.py
@@ -1,11 +1,23 @@
-def Backtrack(self, assignment, off_players, def_players):
-    if off_players.blocking_assignment != None: 
-        return assignment
-    else:
-        for def_players.player_id in off_players.potential_assignment:
-            if off_players.assignment == None: 
-                assign(off_players,def_players)
-                result = Backtrack(assignment, off_players, def_players)
-                if result is not False:
-                    return result
-    return False 
+def backtrack(self):
+        # TODO
+        unassigned_players = 0
+        for ol in self.off_players: # first check if all players have an assignment
+            if (ol.blocking_assignment is None):
+                unassigned_players = unassigned_players + 1
+        if unassigned_players == 0:
+            return self
+        for lineman in self.off_players:
+            lineman_nflId = lineman.player_id
+            if lineman.blocking_assignment is None:
+                defender_nflId = None
+                potential_assignments = lineman.potential_assignments(self.def_players)
+                for defender in potential_assignments:
+                    if defender.blocking_assignment is None:
+                        self.assign(lineman, defender)
+                        result = self.backtrack()
+                        #self.remove_assignment(lineman)
+        else:
+                defender_nflId = lineman.blocking_assignment.player_id
+                results = pd.concat([results, pd.DataFrame(data = [[game, play, frame, lineman_nflId, defender_nflId]], columns = ['gameId', 'playId', 'frameId', 'lineman_nflId', 'defender_nflId'])])
+
+        return results
